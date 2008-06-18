e8bebd9ad9cea214a905a3be8dff16221155bb94
Tommi Virtanen <tv@eagain.net>
2008-06-18 19:57:11 +0300

    In addition to CSS class "current", use "parent" and "active".

--- roast/navi_current.py
+++ roast/navi_current.py
@@ -5,8 +5,17 @@ Flag the current page (and parents) in navigation data.
 def navi_mark_current(navigation, current):
     for item in navigation:
         link = item.get('link')
+
         item['current'] = False
         if link is not None:
             if link == current:
                 item['current'] = True
+
+        item['parent'] = False
+        if link is not None:
+            if current.startswith(link+'/'):
+                item['parent'] = True
+
+        item['active'] = item['current'] or item['parent']
+
         yield item
--- roast/test/test_navi_current.py
+++ roast/test/test_navi_current.py
@@ -29,6 +29,8 @@ def test_simple():
             link='/something-else',
             title='something else',
             current=False,
+            parent=False,
+            active=False,
             ),
         )
     assert_raises(StopIteration, g.next)
@@ -50,6 +52,31 @@ def test_current():
             link='/foo',
             title='foo',
             current=True,
+            parent=False,
+            active=True,
+            ),
+        )
+    assert_raises(StopIteration, g.next)
+
+def test_parent():
+    def g():
+        yield dict(
+            link='/foo',
+            title='foo',
+            )
+    g = navi_current.navi_mark_current(
+        navigation=g(),
+        current='/foo/bar',
+        )
+    got = g.next()
+    eq(
+        got,
+        dict(
+            link='/foo',
+            title='foo',
+            current=False,
+            parent=True,
+            active=True,
             ),
         )
     assert_raises(StopIteration, g.next)
