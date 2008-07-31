12cde7d175ddb7b782bf391c0828c294a6a0df17
Tommi Virtanen <tv@eagain.net>
2008-08-01 02:12:10 +0300

    Move support code closer to where it's used.

--- roast/action/dia.py
+++ roast/action/dia.py
@@ -1,11 +1,24 @@
 import os
+import subprocess
 
-from roast import export_dia
+def export_dia(dia, png):
+    tmp = '%s.tmp' % png
+    returncode = subprocess.call(
+        args=[
+            'dia',
+            '--filter=png-libart',
+            '--export=%s' % tmp,
+            '%s' % dia,
+            ],
+        )
+    if returncode!=0:
+        raise RuntimeError('dia failed with status %d' % returncode)
+    os.rename(tmp, png)
 
 def process(config, src_root, src_relative, dst_root, navigation):
     base, ext = os.path.splitext(src_relative)
     png = os.path.join(dst_root, base+'.png')
-    export_dia.export_dia(
+    export_dia(
         dia=os.path.join(src_root, src_relative),
         png=png,
         )
--- roast/action/graphviz.py
+++ roast/action/graphviz.py
@@ -1,12 +1,38 @@
 import os
+import subprocess
 
-from roast import format_dot
+def format_dot(dot, pdf, png):
+    tmp = '%s.tmp' % pdf
+    returncode = subprocess.call(
+        args=[
+            'dot',
+            '-Tpdf',
+            '-o', tmp,
+            dot,
+            ],
+        )
+    if returncode!=0:
+        raise RuntimeError('dot failed with status %d' % returncode)
+    os.rename(tmp, pdf)
+
+    tmp = '%s.tmp' % png
+    returncode = subprocess.call(
+        args=[
+            'convert',
+            '-resize', '600x600>',
+            'pdf:%s' % pdf,
+            'png:%s' % tmp,
+            ],
+        )
+    if returncode!=0:
+        raise RuntimeError('convert failed with status %d' % returncode)
+    os.rename(tmp, png)
 
 def process_dot(config, src_root, src_relative, dst_root, navigation):
     base, ext = os.path.splitext(src_relative)
     pdf = os.path.join(dst_root, base+'.pdf')
     png = os.path.join(dst_root, base+'.png')
-    format_dot.format_dot(
+    format_dot(
         dot=os.path.join(src_root, src_relative),
         pdf=pdf,
         png=png,
